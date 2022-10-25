adbc8fd70097f7806111407588e0d3f42a42fd1c
Bhuvanavelugula <115540259+Bhuvanavelugula@users.noreply.github.com>
2022-10-25 22:23:45 +0530

    n=int(input()) for i in range(1,n+1):     for j in range(1,i+1):         print(j,end="")     print()

--- print_the_following_pattern.py
+++ print_the_following_pattern.py
@@ -1,8 +1,5 @@
 n=int(input())
-L="ZXCVBNMLKJHGFDSAQWERTYUIOP"
-L=list(L)
-L.sort()
-for i in range(n):
-    for _ in range(n):
-        print(L[i],end=" ")
+for i in range(1,n+1):
+    for j in range(1,i+1):
+        print(j,end="")
     print()
